54893314cb32e472e4fb37a723d3303e476876dc
Matthew Holloway <m.holloway@uqconnect.edu.au>
2020-07-04 19:27:45 +1000

    updating comments

--- riskc/ServerConnect.cpp
+++ riskc/ServerConnect.cpp
@@ -3,39 +3,47 @@
 #include "ServerConnect.h"
 #include "TextBox.h"
 
+
+//server connect constructor
 ServerConnect::ServerConnect(int height, int width) {
+	//defaults name to player
 	name = "player";
+
+	//creates a new font (only using one font on project 
+	// could make this on master object created and passed around
 	font = new sf::Font();
 	if (!(*font).loadFromFile("resources/SourceCodeVariable-Roman.ttf")) {
 		//load another font
 	}
+	//inital strings for text boxes
 	serverIp = "";
 	port = "";
 	name = "";
+
+	//title text
 	title = sf::Text("Server Direct Connect", *font);
 	title.setOrigin(title.getGlobalBounds().width / 2, title.getGlobalBounds().height / 2);
 	title.setPosition(width / 2, height / 5);
 
+	//connect text (need to be made a button
 	connect = sf::Text("Connect", *font);
 	connect.setOrigin(connect.getGlobalBounds().width / 2, connect.getGlobalBounds().height / 2);
 	connect.setPosition(width / 2, height / 2 + height /3);
 
+	//back text (need to be made a button
 	back = sf::Text("Back", *font);
 	back.setOrigin(back.getGlobalBounds().width / 2, back.getGlobalBounds().height / 2);
 	back.setPosition(50, 20);
-
-	int widthProportion = 3;
-	int heightPropotion = 15;
 	
+	//form for server connection
 	enterIP = new TextBox(width / 2 - 250, height / 3, 30, 500, "0.0.0.0");
-
 	enterPort = new TextBox(width / 2 - 250, height / 3 + 100, 30, 500, "0000");
-
 	enterName = new TextBox(width / 2 - 250,  height / 3 + 200, 30, 500, "player1");
 	
-
 }
 
+
+//draw the server connect page elements
 void ServerConnect::draw(sf::RenderWindow& window) {
 	
 	window.draw(title);
@@ -48,6 +56,7 @@ void ServerConnect::draw(sf::RenderWindow& window) {
 	
 }
 
+//connects to the server
 void connect_to_server(std::string ipRaw, std::string port) {
 
 
@@ -62,6 +71,7 @@ void connect_to_server(std::string ipRaw, std::string port) {
 
 }
 
+//handles input for the screen
 void ServerConnect::handleInput(sf::Event event, sf::Vector2i mousePos, StateHolder& state, sf::Cursor& cursor, sf::RenderWindow& window,  void (*change_state)(StateHolder& state, App_State transition)) {
 
 	enterIP->moveCursor(mousePos, event);
